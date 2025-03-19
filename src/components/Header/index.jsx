4b0e08038b0f510c1809c51fd2f1ce0b68cfd423
vineetha <vineethaveerapu4@gmail.com>
2025-03-19 21:06:06 +0100

    added logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,8 @@ import styles from "./Header.module.css";
 import { List, X } from "@phosphor-icons/react";
 import Navigation from "../Navigation";
 import { useState } from "react";
+import logo from "../../assets/tulips.png";
+import { slide as Menu } from "react-burger-menu";
 
 const Header = ({ handleClick }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,7 +21,8 @@ const Header = ({ handleClick }) => {
   return (
     <>
       <div className={styles.header}>
-        <span>Logo</span>
+        <img src={logo} alt="logo" className={styles.logo} />
+
         <Navigation handleClick={handleClick} />
         <button onClick={menuButtonClick} className={styles.mobileMenuButton}>
           {isOpen ? <X size={32} /> : <List size={32} />}
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,10 +1,13 @@
 const Navigation = ({ handleClick }) => {
   return (
-    <nav>
-      <div onClick={handleClick}>Home</div>
-      <div onClick={handleClick}>Garden</div>
-      <div onClick={handleClick}>Services</div>
-    </nav>
+    <>
+      <nav>
+        <div onClick={handleClick}>Home</div>
+        <div onClick={handleClick}>Garden</div>
+        <div onClick={handleClick}>Services</div>
+        <div onClick={handleClick}>Contact</div>
+      </nav>
+    </>
   );
 };
 
