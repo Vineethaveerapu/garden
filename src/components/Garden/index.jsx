309d3f90202e8885dee6987d43d39333eab3d241
vineetha <vineethaveerapu4@gmail.com>
2025-03-22 18:40:16 +0100

    adding background image to garden page

--- src/components/Garden/index.jsx
+++ src/components/Garden/index.jsx
@@ -1,5 +1,13 @@
+import styles from "./Garden.module.css";
+
 const Garden = () => {
-  return <div>Garden</div>;
+  return (
+    <>
+      <div className={styles.gardenContainer}>
+        <h2>Create your dream Garden at Home</h2>
+      </div>
+    </>
+  );
 };
 
 export default Garden;
