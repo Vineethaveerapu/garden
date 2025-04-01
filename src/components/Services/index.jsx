d2399b421452d72e49b4db15f6c8e4e430148b21
vineetha <vineethaveerapu4@gmail.com>
2025-04-01 20:33:23 +0200

    services content and styles

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,5 +1,23 @@
+import gardenServices from "../../data/services";
+import styles from "./Services.module.css";
+import ServiceCard from "../ServiceCard";
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <>
+      <div className={styles.servicesHeader}>
+        <h1>Our Services</h1>
+        <p className={styles.servicesHeaderText}>
+          We offer a wide range of services to help you create and maintain your
+          dream garden.
+        </p>
+      </div>
+      <div className={styles.servicesContainer}>
+        {gardenServices.map((service) => (
+          <ServiceCard key={service.title} service={service} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Services;
